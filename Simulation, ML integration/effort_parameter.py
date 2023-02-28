00b40cd4b346fc246ee36e03ca1d19783eb04361
HalRejali <47081210+HalRejali@users.noreply.github.com>
2023-02-28 14:30:51 +0000

    Update effort_parameter.py

--- Simulation, ML integration/effort_parameter.py
+++ Simulation, ML integration/effort_parameter.py
@@ -3,7 +3,7 @@ import numpy as np
 def holding(self, limb, centre, width):
     return ((1-(((self.simulation_data["pm_space"].bodies[limb].angle - self.simulation_data["pm_space"].bodies[1].angle) - centre) / width))**4) * 90*abs(np.sin(self.simulation_data["pm_space"].bodies[limb].angle))
 
-def get_effort(self, leg_acc, torso_acc):
+def get_effort(self, leg_acc, torso_acc,k4 = 1,k5 = 1,k6 = 1,k7 = 1):
     k4 = 1
     k5 = 1
     k6 = 1
